859598cad7acdd48c06eb114685b1c1fa78b57ca
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-03-15 19:11:59 +0200

    Assignment part 2 - Modify the get function to open the database connection and retrieve data from the database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const cors = require('cors')
+const {Pool} = require('pg');
 
 const app = express();
 app.use(cors());
 const port = 3001
 
-app.get("/",(req,res) => {
+/*app.get("/",(req,res) => {
     res.status(200).json({result:'success'})
+});*/
+
+app.get("/",(req,res) => {
+    const pool = openDb()
+
+    pool.query('SELECT * FROM task' , (error, result) => {
+        if (error){
+            res.status(500).json({error:error.message})
+        }
+        res.status(200).json(result.rows)
+    })
 });
+
+const openDb = () => {
+    const pool = new Pool ({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: '1_oamkekataumagak',
+        port: 5432
+    })
+    return pool
+}
 
 app.listen(port)
