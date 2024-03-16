4fe16e9948781ea18bc5b2267ba97664bd35dd3b
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-03-16 17:43:56 +0200

    Assignment part 3 - completed

--- js/index.js
+++ js/index.js
@@ -1,40 +1,72 @@
+const BACKEND_ROOT_URL = 'http://localhost:3001'
+
 //Get reference to the div element
 let divList = document.querySelector('body');
 //get reference to input element
-let inputElement = document.querySelector('input');
+let input = document.querySelector('input');
 //create new un ordered list element
-let unOrderedList = document.createElement('ul');
-unOrderedList.className = "list-group";
+let list = document.createElement('ul');
+list.className = "list-group";
 //append un ordered list to div element
-divList.appendChild(unOrderedList);
+divList.appendChild(list);
+
+input.disabled = true;
+
+const renderTask = (task) => {
+    const li = document.createElement('li')
+    li.setAttribute('class','list-group-item')
+    li.innerHTML = task
+    list.append(li)
+}
+
+const getTasks = async() => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL)
+        const json = await response.json()
+        json.forEach(task => {
+            renderTask(task.description)
+        })
+        input.disabled = false
+    } catch (error){
+        alert("Error retrieving tasks" + error.message)
+    }
+}
+
+//define function for saving task
+const saveTask = async(task) => {
+    try{
+        const json = JSON.stringify({description: task})
+        const response = await fetch(BACKEND_ROOT_URL + '/new',{
+            method:'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        return response.json()
+    } catch (error) {
+        alert("Error saving task " + error.message)
+    }
+}
 
 //add event listener to input elemnt
-inputElement.addEventListener("keypress",function(event){
+input.addEventListener("keypress",(event) => {
     
     //check the pressed key id "enter" key or not
     if(event.key === "Enter"){
         //prevent reloding the page
         event.preventDefault();
         //get the text content of input field to a variable
-        let text = inputElement.value.trim();
-        //clear the content of input field
-        inputElement.value = "";
+        const task = input.value.trim();
         //check wether the provided input is empty or not
-        if(text != ''){
-            //call "createList" function and assign value to a variable
-            let ulContent = createList(text);
-            //append list to unordered list
-            unOrderedList.appendChild(ulContent);
-        }
-        
+        if(task != ''){
+            saveTask(task).then((json) => {
+                renderTask(task)
+                input.value = ''
+            })
+            
+        }        
     }
 });
 
-//create a function for list element
-function createList(content){
-    let list = document.createElement('li');
-    list.setAttribute("class","list-group-item");
-    //list.className = "list-group-item";
-    list.innerText = content;
-    return list;
-}
+getTasks()
