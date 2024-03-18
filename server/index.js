e0cb01b35eb68acb393a40bda7f4497101997b45
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-03-18 23:12:44 +0200

    Assignment part 6 - completed

--- server/index.js
+++ server/index.js
@@ -1,68 +1,68 @@
+require('dotenv').config()
+//console.log(process.env)
 const express = require('express')
 const cors = require('cors')
-const {Pool} = require('pg');
+//const {Pool} = require('pg');
+const {query} = require('./helpers/db.js')
 
 const app = express();
 app.use(cors());
 app.use(express.json());//reading posted values from theclient as JSON.
 app.use(express.urlencoded({extended: false}))
 
-const port = 3001
+const port = process.env.PORT
 
 /*app.get("/",(req,res) => {
     res.status(200).json({result:'success'})
 });*/
 
-app.get("/",(req,res) => {
-    const pool = openDb()
-
-    pool.query('SELECT * FROM task' , (error, result) => {
-        if (error){
-            res.status(500).json({error:error.message})
-        }
-        res.status(200).json(result.rows)
-    })
+app.get("/",async(req,res) => {
+    console.log(query)
+    try{
+        const result = await query('select * from task')
+        const rows = result.rows ? result.rows : []
+        res.status(200).json(rows)
+    } catch(error){
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error:error})
+    }
 });
 
 const openDb = () => {
     const pool = new Pool ({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: '1_oamkekataumagak',
-        port: 5432
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
     })
     return pool
 }
 
 //Implement a post handler, which is used to receive value(s) from the client and execute insert into statement into the database. SQL contains some parameters ($1) which are provided as an array when calling the query method. 
 
-app.post("/new",(req,res) => {
-    const pool = openDb()
-
-    pool.query('insert into task (description) values ($1) returning *',
-    [req.body.description],
-    (error,result) => {
-        if (error) {
-            res.status(500).json({error: error.message})
-        }   else {
-            res.status(200).json({id : result.rows[0].id})
-        }
-    })
+app.post("/new",async(req,res) => {
+    try{
+        const result = await query('insert into task (description) values ($1) returning *',[req.body.description])
+        res.status(200).json({id:result.rows[0].id})
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
 })
 
 //Implementing deletion functionality to the backend
 app.delete("/delete/:id",async(req,res) => {
-    const pool = openDb()
-    const id = parseInt(req.params.id)
-    pool.query('delete from task where id = $1',
-    [id],
-    (error,result) => {
-        if(error) {
-            res.status(500).json({error: error.message})
-        } else {
-            res.status(200).json({id:id})
-        }
-    })
+    const id = Number(req.params.id)
+    try{
+        const result = await query('delete from task where id = $1',[id])
+        res.status(200).json({id:id})
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.sendStatus(500).json({error:error})
+    }
 })
 app.listen(port)
